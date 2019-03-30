a78c6a083eb83efff393c080576bb4a77a116ff7
Thainá Flexa <thainaeliflexa@gmail.com>
2019-03-30 00:19:29 -0300

    Alterei o arquivo .h

--- Arqueiro.h
+++ Arqueiro.h
@@ -1,11 +1,20 @@
 #ifndef ARQUEIRO_H
 #define ARQUEIRO_H
+#include <string>
+using std::string;
+
 
 class Arqueiro
 {
+
+
 public:
     Arqueiro();
     ~Arqueiro();
+    
+private:
+    int pontuacao;
+
 
 };
 
